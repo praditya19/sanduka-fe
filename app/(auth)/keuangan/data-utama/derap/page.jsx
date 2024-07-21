09bc68121944d14cab08c699e42bbca9bcfa4687
praditya19 <praditya.rendi88@gmail.com>
2024-07-21 09:29:16 +0700

    perbaikan iuran pgri, daspen, dan done derap

--- app/(auth)/keuangan/data-utama/derap/page.jsx
+++ app/(auth)/keuangan/data-utama/derap/page.jsx
@@ -1,7 +1,286 @@
-import React from "react";
+"use client";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
-function page() {
-  return <div>page derap</div>;
+function DerapForm() {
+  const [provinsi, setProvinsi] = useState(5700);
+  const [kabupaten, setKabupaten] = useState(9500);
+  const [cabang, setCabang] = useState(1350);
+  const [totalHarga, setTotalHarga] = useState(0);
+  const [totalHargaAkhir, setTotalHargaAkhir] = useState(0);
+  const [jumlahPesanan, setJumlahPesanan] = useState(0);
+  const [setorProvinsi, setSetorProvinsi] = useState(0);
+  const [untukKabupaten, setUntukKabupaten] = useState(0);
+  const [untukCabang, setUntukCabang] = useState(0);
+  const [jenisCabang, setJenisCabang] = useState("");
+
+  const handleProvinsiChange = (e) => {
+    setProvinsi(e.target.value);
+  };
+
+  const handleKabupatenChange = (e) => {
+    setKabupaten(e.target.value);
+  };
+
+  const handleCabangChange = (e) => {
+    setCabang(e.target.value);
+  };
+
+  const handleJumlahPesananChange = (e) => {
+    setJumlahPesanan(e.target.value);
+  };
+
+  const handleJenisCabangChange = (e) => {
+    setJenisCabang(e.target.value);
+  };
+
+  const calculateTotalHarga = () => {
+    const hargaProvinsi = parseInt(provinsi) || 0;
+    const hargaKabupaten = parseInt(kabupaten) || 0;
+    const hargaCabang = parseInt(cabang) || 0;
+    const jumlahPesananInt = parseInt(jumlahPesanan) || 1;
+
+    const setorProvinsiTotal = hargaProvinsi * jumlahPesananInt;
+    const untukKabupatenTotal = hargaKabupaten * jumlahPesananInt;
+    const untukCabangTotal = hargaCabang * jumlahPesananInt;
+
+    const total = hargaProvinsi + hargaKabupaten + hargaCabang;
+    setTotalHarga(total);
+
+    const totalAkhir =
+      setorProvinsiTotal + untukKabupatenTotal + untukCabangTotal;
+    setTotalHargaAkhir(totalAkhir);
+
+    setSetorProvinsi(setorProvinsiTotal);
+    setUntukKabupaten(untukKabupatenTotal);
+    setUntukCabang(untukCabangTotal);
+  };
+
+  const resetForm = () => {
+    setProvinsi(5700);
+    setKabupaten(9500);
+    setCabang(1350);
+    setTotalHarga(0);
+    setJumlahPesanan(0);
+    setSetorProvinsi(0);
+    setUntukKabupaten(0);
+    setUntukCabang(0);
+    setTotalHargaAkhir(0);
+    setJenisCabang("");
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      calculateTotalHarga();
+      e.preventDefault();
+    }
+  };
+
+  return (
+    <div className="container mx-auto p-6">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="p-8 rounded-lg shadow-lg border border-gray-200 bg-white">
+          <h2 className="bg-blue-500 text-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 text-center">
+            Pesanan Derap PGRI
+          </h2>
+          <div className="space-y-6">
+            {[
+              {
+                id: "provinsi",
+                label: "Derap Provinsi",
+                value: provinsi,
+                onChange: handleProvinsiChange,
+              },
+              {
+                id: "kabupaten",
+                label: "Derap Kabupaten",
+                value: kabupaten,
+                onChange: handleKabupatenChange,
+              },
+              {
+                id: "cabang",
+                label: "Derap Cabang/Ranting",
+                value: cabang,
+                onChange: handleCabangChange,
+              },
+              {
+                id: "totalHarga",
+                label: "Total Harga",
+                value: totalHarga,
+                readOnly: true,
+                customClass: "bg-gray-200",
+              },
+            ].map((field) => (
+              <div
+                key={field.id}
+                className="flex flex-col lg:flex-row items-center mb-4"
+              >
+                <Label
+                  htmlFor={field.id}
+                  className="block text-gray-800 text-lg font-semibold mb-2 lg:mb-0 lg:w-full"
+                >
+                  {field.label}
+                </Label>
+                <Input
+                  type="number"
+                  id={field.id}
+                  className={`w-full px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-150 ease-in-out lg:ml-4 ${
+                    field.customClass || ""
+                  }`}
+                  onKeyPress={handleKeyPress}
+                  value={field.value}
+                  onChange={field.onChange}
+                  readOnly={field.readOnly}
+                />
+              </div>
+            ))}
+
+            <div className="flex items-center mb-4">
+              <Label
+                htmlFor="jumlahPesanan"
+                className="block text-gray-800 text-lg font-semibold mb-2 lg:mb-0 w-full"
+              >
+                Jumlah Pesanan
+              </Label>
+              <select
+                id="jenisCabang"
+                className="w-2/4 px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-150 ease-in-out ml-14"
+                value={jenisCabang}
+                onChange={handleJenisCabangChange}
+              >
+                <option></option>
+                <option value="1">Cabang 1</option>
+                <option value="2">Cabang 2</option>
+                <option value="3">Cabang 3</option>
+                <option value="4">Cabang 4</option>
+                <option value="5">Cabang 5</option>
+              </select>
+              <Input
+                type="number"
+                id="jumlahPesananInput"
+                className="w-2/4 px-4 py-3 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-150 ease-in-out ml-4"
+                value={jumlahPesanan}
+                onChange={handleJumlahPesananChange}
+                onKeyPress={handleKeyPress}
+              />
+            </div>
+
+            <div className="flex justify-center space-x-4 mt-6">
+              <Button
+                className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-lg focus:outline-none focus:ring-4 focus:ring-blue-300 transition duration-150 ease-in-out"
+                onClick={calculateTotalHarga}
+              >
+                Simpan
+              </Button>
+              <Button
+                className="bg-red-500 text-white px-6 py-2 rounded-md shadow-md hover:bg-red-600 transition duration-150 ease-in-out"
+                onClick={resetForm}
+              >
+                Reset
+              </Button>
+            </div>
+          </div>
+        </div>
+
+        <div className="p-6 rounded-lg shadow-md border border-gray-200 bg-white">
+          <div className="mb-4">
+            <h3 className="text-lg font-bold mb-2 text-blue-600">
+              Jumlah Pesanan :
+            </h3>
+            <p className="text-lg text-gray-700">
+              <b>{jumlahPesanan} eksemplar</b>
+            </p>
+          </div>
+          <div className="mb-4">
+            <h3 className="text-lg font-bold mb-2 text-blue-600">
+              Setor Provinsi:
+            </h3>
+            <p className="text-lg text-gray-700">
+              <b>Rp. {setorProvinsi.toLocaleString()}</b>
+            </p>
+          </div>
+          <div className="mb-4">
+            <h3 className="text-lg font-bold mb-2 text-blue-600">
+              Untuk Kabupaten:
+            </h3>
+            <p className="text-lg text-gray-700">
+              <b>Rp. {untukKabupaten.toLocaleString()}</b>
+            </p>
+          </div>
+          <div className="mb-4">
+            <h3 className="text-lg font-bold mb-2 text-blue-600">
+              Untuk Cabang:
+            </h3>
+            <p className="text-lg text-gray-700">
+              <b>Rp. {untukCabang.toLocaleString()}</b>
+            </p>
+          </div>
+          <div>
+            <h3 className="text-lg font-bold mb-2 text-blue-600">Total:</h3>
+            <p className="text-lg text-gray-700">
+              <b>Rp. {totalHargaAkhir.toLocaleString()}</b>
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-teal-800 p-2 rounded-lg shadow-lg mt-5">
+        <div className="flex flex-col sm:flex-row sm:justify-between items-center mb-4">
+          <div className="flex flex-wrap gap-4 mb-4 sm:mb-0 px-5 mt-5">
+            <select className="shadow-lg border rounded w-1/2 sm:w-auto py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white">
+              <option>Tampil Semua</option>
+            </select>
+            <select className="shadow-lg border rounded w-1/2 sm:w-auto py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white">
+              <option>Juli</option>
+              <option>Agustus</option>
+              <option>September</option>
+            </select>
+            <select className="shadow-lg border rounded w-1/2 sm:w-auto py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white">
+              <option>2023</option>
+              <option>2024</option>
+              <option>2025</option>
+            </select>
+          </div>
+          <h1 className="text-2xl font-bold text-white mb-4 sm:mb-0 mt-4">
+            Transaksi Juli 2024
+          </h1>
+          <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded transition duration-300 ease-in-out mt-3 mr-6 w-24">
+            Cetak
+          </Button>
+        </div>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="min-w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-sm text-gray-700 uppercase bg-gray-100 dark:bg-gray-800 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                No
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Cabang Khusus
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Pesanan
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Total
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <td className="px-6 py-4">1</td>
+              <td className="px-6 py-4">Jumlah</td>
+              <td className="px-6 py-4">0</td>
+              <td className="px-6 py-4">0</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default page;
+export default DerapForm;
