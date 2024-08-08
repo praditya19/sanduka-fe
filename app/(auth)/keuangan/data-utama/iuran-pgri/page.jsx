6757a30df2efdc41fc1300b854b168ffb18e47a6
praditya19 <praditya.rendi88@gmail.com>
2024-08-08 16:20:49 +0700

    update iuran pgri

--- app/(auth)/keuangan/data-utama/iuran-pgri/page.jsx
+++ app/(auth)/keuangan/data-utama/iuran-pgri/page.jsx
@@ -252,6 +252,7 @@ export default function Iuran() {
   const [totalIuran, setTotalIuran] = useState(6000);
   const [totalSumbangan, setTotalSumbangan] = useState(9000);
   const [filteredData, setFilteredData] = useState(TableData);
+  const [isFormVisible, setFormVisible] = useState(false);
 
   useEffect(() => {
     const total = iuranPB + iuranProvinsi + iuranKabupaten + iuranCabang;
@@ -396,135 +397,155 @@ export default function Iuran() {
     printWindow.close();
   };
 
+  const handleToggleForm = () => {
+    setFormVisible(!isFormVisible);
+  };
+
   return (
     <div className="container mx-auto p-6 bg-gray-50 rounded-lg shadow-lg">
+      <Button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleToggleForm}
+      >
+        Rincian Iuran
+      </Button>
+
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
         <div className="bg-white p-6 rounded-lg shadow-md">
           <h2 className="bg-teal-700 text-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-5 text-center">
             Besaran Iuran PGRI
           </h2>
-          <div className="mb-4">
-            <Label
-              htmlFor="iuranPB"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Iuran PB
-            </Label>
-            <Input
-              type="text"
-              id="iuranPB"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={formatRupiah(iuranPB)}
-              onChange={(event) => handleInputChange(event, setIuranPB)}
-            />
-          </div>
-          <div className="mb-4">
-            <Label
-              htmlFor="iuranProvinsi"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Iuran Provinsi
-            </Label>
-            <Input
-              type="text"
-              id="iuranProvinsi"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={formatRupiah(iuranProvinsi)}
-              onChange={(event) => handleInputChange(event, setIuranProvinsi)}
-            />
-          </div>
-          <div className="mb-4">
-            <Label
-              htmlFor="iuranKabupaten"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Iuran Kabupaten
-            </Label>
-            <Input
-              type="text"
-              id="iuranKabupaten"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={formatRupiah(iuranKabupaten)}
-              onChange={(event) => handleInputChange(event, setIuranKabupaten)}
-            />
-          </div>
-          <div className="mb-4">
-            <Label
-              htmlFor="iuranCabang"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Iuran Cabang/Ranting
-            </Label>
-            <Input
-              type="text"
-              id="iuranCabang"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={formatRupiah(iuranCabang)}
-              onChange={(event) => handleInputChange(event, setIuranCabang)}
-            />
-          </div>
-          <div className="mb-4">
-            <Label
-              htmlFor="totalIuran"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Total Iuran
-            </Label>
-            <Input
-              type="text"
-              id="totalIuran"
-              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                totalIuran === totalIuran ? "bg-gray-200" : ""
-              }`}
-              value={formatRupiah(totalIuran)}
-              readOnly
-            />
-          </div>
-          <div className="mb-4">
-            <Label
-              htmlFor="sumbanganSanduka"
-              className="block text-gray-700 text-sm font-bold mb-2"
-            >
-              Sumbangan Sanduka
-            </Label>
-            <Input
-              type="text"
-              id="sumbanganSanduka"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              value={formatRupiah(sumbanganSanduka)}
-              onChange={(event) =>
-                handleInputChange(event, setSumbanganSanduka)
-              }
-            />
-          </div>
-          <div className="mb-4">
-            <h2 className="bg-teal-700 text-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-1 text-center">
-              Total Sumbangan dan Iuran PGRI
-            </h2>
-            <Input
-              type="text"
-              id="totalSumbangan"
-              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
-                totalSumbangan === totalIuran + sumbanganSanduka
-                  ? "bg-gray-200"
-                  : ""
-              }`}
-              value={formatRupiah(totalSumbangan)}
-              readOnly
-            />
-          </div>
-          <div className="flex justify-center space-x-2">
-            <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-              Simpan
-            </Button>
-            <Button
-              className="bg-red-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              onClick={handleReset}
-            >
-              Reset
-            </Button>
-          </div>
+          {isFormVisible && (
+            <div>
+              <div className="mb-4">
+                <Label
+                  htmlFor="iuranPB"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Iuran PB
+                </Label>
+                <Input
+                  type="text"
+                  id="iuranPB"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  value={formatRupiah(iuranPB)}
+                  onChange={(event) => handleInputChange(event, setIuranPB)}
+                />
+              </div>
+              <div className="mb-4">
+                <Label
+                  htmlFor="iuranProvinsi"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Iuran Provinsi
+                </Label>
+                <Input
+                  type="text"
+                  id="iuranProvinsi"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  value={formatRupiah(iuranProvinsi)}
+                  onChange={(event) =>
+                    handleInputChange(event, setIuranProvinsi)
+                  }
+                />
+              </div>
+              <div className="mb-4">
+                <Label
+                  htmlFor="iuranKabupaten"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Iuran Kabupaten
+                </Label>
+                <Input
+                  type="text"
+                  id="iuranKabupaten"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  value={formatRupiah(iuranKabupaten)}
+                  onChange={(event) =>
+                    handleInputChange(event, setIuranKabupaten)
+                  }
+                />
+              </div>
+              <div className="mb-4">
+                <Label
+                  htmlFor="iuranCabang"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Iuran Cabang/Ranting
+                </Label>
+                <Input
+                  type="text"
+                  id="iuranCabang"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  value={formatRupiah(iuranCabang)}
+                  onChange={(event) => handleInputChange(event, setIuranCabang)}
+                />
+              </div>
+              <div className="mb-4">
+                <Label
+                  htmlFor="totalIuran"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Total Iuran
+                </Label>
+                <Input
+                  type="text"
+                  id="totalIuran"
+                  className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                    totalIuran === totalIuran ? "bg-gray-200" : ""
+                  }`}
+                  value={formatRupiah(totalIuran)}
+                  readOnly
+                />
+              </div>
+              <div className="mb-4">
+                <Label
+                  htmlFor="sumbanganSanduka"
+                  className="block text-gray-700 text-sm font-bold mb-2"
+                >
+                  Sumbangan Sanduka
+                </Label>
+                <Input
+                  type="text"
+                  id="sumbanganSanduka"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  value={formatRupiah(sumbanganSanduka)}
+                  onChange={(event) =>
+                    handleInputChange(event, setSumbanganSanduka)
+                  }
+                />
+              </div>
+              <div className="mb-4">
+                <h2 className="bg-teal-700 text-2xl text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-1 text-center">
+                  Total Sumbangan dan Iuran PGRI
+                </h2>
+                <Input
+                  type="text"
+                  id="totalSumbangan"
+                  className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${
+                    totalSumbangan === totalIuran + sumbanganSanduka
+                      ? "bg-gray-200"
+                      : ""
+                  }`}
+                  value={formatRupiah(totalSumbangan)}
+                  readOnly
+                />
+              </div>
+              <div className="flex justify-center space-x-2">
+                <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                  Simpan
+                </Button>
+                <Button
+                  className="bg-red-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  onClick={handleReset}
+                >
+                  Reset
+                </Button>
+              </div>
+            </div>
+          )}
         </div>
+
         <div className="bg-white p-4 rounded-lg shadow-md">
           <h3 className="text-lg font-bold mb-2">Jumlah Anggota: 6938</h3>
           <h3 className="text-lg font-bold mb-2">
