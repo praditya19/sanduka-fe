e3ce280382b569df6f18600c2a59f66ba3142700
Fausta Rizky Abriansah <semarangkiki40@gmail.com>
2024-08-13 22:15:52 +0700

    perbaikan name komponen ui

--- app/(auth)/pengaturan/tambah/page.jsx
+++ app/(auth)/pengaturan/tambah/page.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -267,12 +266,12 @@ const page = () => {
                   </Select>
                 </div>
                 <div className="flex justify-end space-x-2">
-                  <button
+                  <Button
                     type="submit"
                     className="bg-blue-500 text-white px-4 py-2 rounded"
                   >
                     Tambah
-                  </button>
+                  </Button>
                 </div>
               </div>
             </div>
