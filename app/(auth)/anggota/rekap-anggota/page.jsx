fa31bb7bdad99cae324212c0632def89b61dfe67
Praditya Rendi <43976106+praditya19@users.noreply.github.com>
2024-08-25 21:19:02 +0700

    Update page.jsx

--- app/(auth)/anggota/rekap-anggota/page.jsx
+++ app/(auth)/anggota/rekap-anggota/page.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { membersData } from "../data.js";
-import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
