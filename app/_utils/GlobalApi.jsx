d86052d7c07a5c77641d756efccc273b6b1120c5
praditya19 <praditya.rendi88@gmail.com>
2024-12-02 12:10:58 +0700

    update env

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ReceiptEuro } from "lucide-react";
 
 const axiosClient = axios.create({
-  baseURL: "https://dfd4-103-134-212-202.ngrok-free.app",
+  baseURL: "https://c751-103-134-212-202.ngrok-free.app",
   headers: {
     "ngrok-skip-browser-warning": "true",
   },
