42f9c9d416356fd08e042d0e71d46fa44e8e119d
Praditya Rendi <43976106+praditya19@users.noreply.github.com>
2024-12-27 18:59:40 +0700

    Update GlobalApi.jsx

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ReceiptEuro } from "lucide-react";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://sanduka.my.id",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
