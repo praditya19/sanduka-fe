317a237b698a1699eb0dd7005a72ba32aa412305
praditya19 <praditya.rendi88@gmail.com>
2025-01-13 10:27:49 +0700

    update files

--- app/_components/HeaderMenu.jsx
+++ app/_components/HeaderMenu.jsx
@@ -209,8 +209,6 @@ const HeaderHome = () => {
     };
   }, [isProfileMenuOpen]);
 
-  console.log(profileImageUrl);
-
   return (
     <nav className="bg-teal-500 shadow-md fixed top-0 inset-x-0 z-50">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
