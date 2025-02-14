5abc7948790dec8a07061b4ccd03e79a8fc69977
praditya19 <praditya.rendi88@gmail.com>
2025-02-14 15:05:25 +0700

    menambahkan button delete di unit kerja dan dropdown nama ranting

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -803,6 +803,7 @@ const hapusPemasukanUangMasuk = async (id) => {
     throw error;
   }
 };
+
 const getDefaultIuranById = async (id) => {
   try {
     const response = await axiosClient.get(`/api/defaultIuran/${id}`);
@@ -945,6 +946,7 @@ const createKalenderData = async (payload) => {
     throw error;
   }
 };
+
 const createTargetKalender = async (payload) => {
   try {
     const response = await axiosClient.post("/api/target-kalender", payload);
@@ -1822,6 +1824,35 @@ const uploadFileRegister = async (formData) => {
   }
 };
 
+const deleteUnitKerjaRanting = async (unitKerjaIds) => {
+  try {
+    const response = await axiosClient.delete(
+      "/api/ranting/deleteByUnitKerja",
+      {
+        params: { unitKerjaIds },
+        paramsSerializer: (params) => {
+          return new URLSearchParams(params).toString();
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error("Error deleting unit kerja: ", error);
+    throw error;
+  }
+};
+
+const deleteNamaRanting = async (id) => {
+  try {
+    const response = await axiosClient.delete(`/api/nama-ranting/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error delete data by id", error);
+    throw error;
+  }
+};
+
 // Export all functions
 export default {
   registerUser,
@@ -1952,4 +1983,6 @@ export default {
   getNamaRantingByCabang,
   getNominalAggregatedData,
   uploadFileRegister,
+  deleteUnitKerjaRanting,
+  deleteNamaRanting,
 };
