8e1b54e5d1608d3f673faa1db9b56e1d3daddf3b
Nda22Kurniawan <nda22kurniawan@gmail.com>
2025-02-24 00:17:40 +0700

    fixed

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -1742,6 +1742,7 @@ const getCekHistoryData = async (
   }
 };
 
+//Gallery and Event
 const createSidebarGallery = async (data) => {
   try {
     const formData = new FormData();
@@ -1886,6 +1887,7 @@ const deleteNamaRanting = async (id) => {
 
 const getRekapRanting = () => axiosClient.get("/api/ranting/summary");
 
+//Peserta Event
 const addPesertaEvent = async (pesertaEvent) => {
   try {
     const response = await axiosClient.post("/api/event", pesertaEvent);
