a5972a41c6732b9e27a3b4d5cf04086413abb908
Fausta Rizky Abriansah <semarangkiki40@gmail.com>
2026-05-20 16:41:33 +0700

    interasi rombah rekapitulasi di transaksi bank

--- app/(auth)/transaksi-bank/components/rekapitulasi/RekapTable.jsx
+++ app/(auth)/transaksi-bank/components/rekapitulasi/RekapTable.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const RekapTable = ({ dataRekapitulasi, formatRupiah }) => {
   return (
